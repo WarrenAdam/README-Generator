88cf2c54943d77ba8a7433f49711165437be7de9
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-06-30 10:59:28 -0400

    added packages and paths needed for application,created write README function and function to intialize app with questions generated for README

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// TODO: Create an array of questions for user input
-const questions = [];
-inquirer
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
+  
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(path.join(__dirname, '/Samples/', fileName),generateMarkdown (data), (err) =>
+  err ? console.error(err) : console.log('Success!')
+);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer
   .prompt([
     {
       type: 'input',
@@ -20,7 +33,7 @@ inquirer
     {
       type: 'input',
       message: 'What is your project name?',
-      name: 'project name',
+      name: 'projectName',
     },
     {
       type: 'input',
@@ -36,42 +49,35 @@ inquirer
     },
     {
       type: 'input',
-      message: 'what command do you use to install dependencies? (npm install)',
+      message: 'what command do you use to install dependencies?',
       name: 'dependency',
+      default: 'npm install'
     },
     
     {
       type: 'input',
-      message: 'what command do you use to tests? (npm run test)',
+      message: 'what command do you use to tests?',
       name: 'test',
+      default: 'npm run test',
     },
     
     {
       type: 'input',
       message: 'what does the user need to know about your repository?',
-      name: 'repository info',
+      name: 'repositoryInfo',
     },
 
     {
       type: 'input',
       message: 'what does the user need to know about contributing to your repository?',
-      name: 'Contribution',
+      name: 'contribution',
     },
   ])
-  
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-  fs.readFile('README.md', 'utf8', (error, data) =>
-  error ? console.error(error) : console.log(data))
-
-  fs.writeFile('README.md', process.argv[2], (err) =>
-  err ? console.error(err) : console.log('Success!')
-);
+  .then(function(answers) {
+    console.log(answers);
+    writeToFile('README.md', answers);
+  })
 }
-
-// TODO: Create a function to initialize app
-function init() {}
 
 // Function call to initialize app
 init();

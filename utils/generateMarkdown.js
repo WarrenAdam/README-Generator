c6e0410c41945fd986ceb45cf79cbd3333a0fcc1
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-06-30 11:01:00 -0400

    fixed function to create a badge and license link, creted function for license section and markdown generation of README.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,40 +1,42 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+//  Create a function that returns a license badge and license link based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license) {
-    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]`, 
-    `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`, 
-    `![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)]`, 
-    `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]`;
+  
+    if(license === 'MIT') {
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    }
+    else if (license === 'APACHE 2.0') {
+      return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    }
+    else if (license === 'GPL 3.0') {
+      return `![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+    }
+    else if (license === 'BSD 3') {
+      return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+    }
+    else {
+      return '';
+    }
   }
-   else return '';
-}
+  
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license) {
-    return `(https://opensource.org/licenses/BSD-3-Clause)`, 
-    `(https://opensource.org/licenses/Apache-2.0)`, 
-    `(https://www.gnu.org/licenses/gpl-3.0)`, 
-    `(https://opensource.org/licenses/MIT)`
-
-  }
-  else return '';
-}
-
-// TODO: Create a function that returns the license section of README
+// Created a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license) {
-    return `##License\n\n${(renderLicenseSection(data.license))}`
-  }
-  else return '';
-}
+  if (license === 'none') {
+    return '';
 
-// TODO: Create a function to generate markdown for README
+  }
+  else {
+    return `## License 
+    ${license}`
+  } ;
+} 
+
+//  Created a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title} ${renderLicenseBadge(data.license)}
+  return `# ${data.projectName}
+   ${renderLicenseBadge(data.license)}
 
   ## Description
   
@@ -51,30 +53,30 @@ function generateMarkdown(data) {
   
   ## Installation
   
-  ${data.installation}
+  ${data.dependency}
   
   ## Usage
   
-  ${data.usage}
+  ${data.repositoryInfo}
   
   ## Contributing
   
-  ${data.contributing}
+  ${data.contribution}
   
   ## Tests
   
-  ${data.tests}
+  ${data.test}
   
   ${renderLicenseSection(data.license)}
   
-  ${renderLicenseLink(data.license)}
+ 
   
   ## Contact
   
   For questions or feedback, please feel free to reach out:
   
   - Email: ${data.email}
-  - GitHub: [${data.github}](https://github.com/${data.github})`;
+  - GitHub: [${data.username}](https://github.com/${data.username})`;
 }
 
-module.exports = generateMarkdown();
+module.exports = generateMarkdown;
